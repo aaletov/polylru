5000588c1f513d2afa8f585c52931c441d6bf4cd
Artemy Pozdnyakov <aapozdnyakove@gmail.com>
2023-07-09 16:59:28 +0300

    Add Release and Debug recipes
    Signed-off-by: Artemy Pozdnyakov <aapozdnyakove@gmail.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
-conan install . --output-folder cmake-build-release --build=missing
-cmake . -DCMAKE_TOOLCHAIN_FILE=cmake-build-release/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+set -e
+# conan install . --output-folder cmake-build-release --build=missing
+# mkdir -p Debug Release
+conan install . --output-folder cmake-build-release --settings=build_type=Release
+conan install . --output-folder cmake-build-debug --settings=build_type=Debug
+
+cd cmake-build-release
+cmake -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -S./..
 cmake --build .
+
+cd ./..
+
+cd cmake-build-debug
+cmake -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug -S./..
+cmake --build .
+
+
+
+# cmake --build . --config Debug
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+cd cmake-build-debug
 ctest .
+cd ..
 python3 reporter.py -i ./reports -o ./latex-report/data
